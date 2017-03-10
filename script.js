2fae2819d6c4602a9c0e87acf85a5246ba280ea3
Ariana Golaub <jamindianpochantas@yahoo.com>
2017-03-10 14:41:46 +0000

    script

--- script.js
+++ script.js
@@ -1,7 +1,17 @@
 TinyTurtle.apply(window);
 
 // Type your directions to Tiny Turtle below
-
-
-
+forward(40);
+right(60);
+forward(40);
+right(60);
+forward(40);
+right(60);
+forward(40);
+right(90);
+forward(70);
+right(90);
+forward(40);
+right(90);
+forward(60);
 stamp();
